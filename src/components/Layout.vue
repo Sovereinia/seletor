bb9d41ea6b618da9c8519022bbdb2d9de670d3e8
Grazi <diehlgraziela@gmail.com>
2025-04-26 18:18:03 -0300

    update layout page with categories and features components

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import features from '../data/features';
+import categories from '../data/categories';
+
+import Categories from './Categories.vue';
+import Features from './Features.vue';
+</script>
 
 <template>
   <div class="drawer lg:drawer-open">
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
 
     <main class="drawer-content flex flex-col items-center justify-center">
-      <!-- Page content here -->
       <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden"> Open drawer </label>
       <slot name="content" />
     </main>
@@ -17,70 +22,13 @@
         <input type="text" placeholder="Pesquisar..." class="input" />
 
         <div class="flex flex-col gap-4">
-          <div class="flex flex-col">
-            <div class="divider">Categorias</div>
-            <label for="social_media" class="mb-4">
-              <input
-                type="radio"
-                name="category"
-                id="social_media"
-                value="social_media"
-                class="radio radio-primary"
-                checked
-              />
-              Redes sociais
-            </label>
-            <label for="professional_solutions">
-              <input
-                type="radio"
-                name="category"
-                id="professional_solutions"
-                value="professional_solutions"
-                class="radio radio-primary"
-              />
-              Soluções profissionais
-            </label>
-          </div>
-
-          <ul>
-            <div class="divider">Core Features</div>
-            <li>
-              <label>
-                <input type="checkbox" class="checkbox checkbox-primary" />
-                Timelines públicas
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" class="checkbox checkbox-primary" />
-                Mensagens privadas
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" class="checkbox checkbox-primary" />
-                Criptografia ponta-a-ponta
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" class="checkbox checkbox-primary" />
-                Chats em grupo
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" class="checkbox checkbox-primary" />
-                Chamadas de voz/vídeo
-              </label>
-            </li>
-            <li>
-              <label>
-                <input type="checkbox" class="checkbox checkbox-primary" />
-                Compartilhamento de arquivos
-              </label>
-            </li>
-          </ul>
+          <Categories :items="categories" />
+          <Features
+            v-for="feature in features"
+            :key="feature.id"
+            :title="feature.title"
+            :items="feature.items"
+          />
         </div>
       </aside>
     </div>
