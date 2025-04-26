df286eeb6dd3d9cd935a6da02d717f2732afa790
Grazi <diehlgraziela@gmail.com>
2025-04-26 18:17:48 -0300

    categories component

---
+++ src/components/Categories.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Wrapper from './Wrapper.vue';
+
+defineProps<{
+  items: {
+    id: string;
+    label: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <Wrapper title="Categorias">
+    <div class="flex flex-col gap-2">
+      <label v-for="item in items" :for="item.id" class="text-base-content opacity-80">
+        <input
+          :id="item.id"
+          :value="item.id"
+          :checked="item.id === 'social_media'"
+          type="radio"
+          name="category"
+          class="radio radio-primary mr-1"
+        />
+        {{ item.label }}
+      </label>
+    </div>
+  </Wrapper>
+</template>
+
+<style scoped></style>
