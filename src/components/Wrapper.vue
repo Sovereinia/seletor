bf0e8f90139a82fac359fff225922db2d2ac6925
Grazi <diehlgraziela@gmail.com>
2025-04-26 17:44:20 -0300

    wrapper component

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="divider">{{ title }}</div>
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
