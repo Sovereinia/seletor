35b2a8e4184a811954899538f088823b96b499c7
Grazi <diehlgraziela@gmail.com>
2025-04-26 17:44:52 -0300

    sidebar features component

---
+++ src/components/Features.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Wrapper from './Wrapper.vue';
+
+defineProps<{
+  title: string;
+  items: {
+    id: string;
+    label: string;
+  }[];
+}>();
+</script>
+
+<template>
+  <Wrapper :title="title">
+    <ul>
+      <li v-for="item in items" key="item.id" class="mb-2">
+        <label>
+          <input type="checkbox" class="checkbox checkbox-primary mr-1" />
+          {{ item.label }}
+        </label>
+      </li>
+    </ul>
+  </Wrapper>
+</template>
+
+<style scoped></style>
