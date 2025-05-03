30b9d6cd803361add518172967690376b6225f01
Grazi <diehlgraziela@gmail.com>
2025-05-02 22:15:33 -0300

    default route

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
       path: '/seletor',
       name: 'seletor',
       component: HomeView,
