5df9ed198a33b07fa6b7af3c39c56b4260be8a8b
Viny Brun Kriesang <viniciusbrun.dev@gmail.com>
2025-05-17 18:08:52 +0000

    Update categories.ts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,8 +2,8 @@ import type { Category } from '@/types';
 
 export const categories: Category[] = [
   { id: 'all', name: 'Todos' },
-  { id: 'social', name: 'Redes Sociais' },
+  { id: 'social', name: 'Sociais' },
   { id: 'messaging', name: 'Mensageiros' },
-  { id: 'tools', name: 'Ferramentas Profissionais' },
-  { id: 'protocols', name: 'Protocolos Abertos' },
+  { id: 'tools', name: 'Profissionais' },
+  { id: 'protocols', name: 'Protocolos' },
 ];
