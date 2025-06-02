bf3488e98a64ee3b05db6595a6476e6c5fc2a132
vinybk <viniciusbrun.dev@gmail.com>
2025-06-02 19:03:23 -0300

    a few tweaks to the PR. more info in the comment

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,18 +1,8 @@
-<template>
-  <div class="language-switcher">
-    <select v-model="currentLocale" @change="changeLanguage" class="select select-bordered w-full max-w-xs">
-      <option value="pt">{{ $t('common.portuguese') }}</option>
-      <option value="en">{{ $t('common.english') }}</option>
-      <option value="es">{{ $t('common.spanish') }}</option>
-    </select>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-const { locale } = useI18n()
+const { locale, fallbackLocale, messages } = useI18n()
 const currentLocale = ref(locale.value)
 
 const changeLanguage = () => {
@@ -20,13 +10,35 @@ const changeLanguage = () => {
   localStorage.setItem('user-locale', currentLocale.value)
 }
 
-// Initialize from localStorage if available
+// Initialize from localStorage if available, otherwise from browser
 const savedLocale = localStorage.getItem('user-locale')
+const supportedLocales = Object.keys(messages.value)
+
 if (savedLocale) {
   currentLocale.value = savedLocale
   locale.value = savedLocale
+} else {
+  const browserLocale = navigator.language.split('-')[0]
+  if (supportedLocales.includes(browserLocale)) {
+    currentLocale.value = browserLocale
+    locale.value = browserLocale
+  } else {
+    currentLocale.value = fallbackLocale.value as string
+    locale.value = fallbackLocale.value as string
+  }
 }
 </script>
+
+<template>
+  <div class="language-switcher">
+    <select v-model="currentLocale" @change="changeLanguage" class="bg-transparent appearance-none focus:outline-none text-sm">
+      <option value="pt">{{ $t('common.portuguese') }}</option>
+      <option value="en">{{ $t('common.english') }}</option>
+      <option value="es">{{ $t('common.spanish') }}</option>
+    </select>
+  </div>
+</template>
+
 
 <style scoped>
 .language-switcher {
